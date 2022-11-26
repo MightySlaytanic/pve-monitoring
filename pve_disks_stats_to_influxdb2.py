5c2a942ed8b4b427e644bd3e8317830b8676c8db
MightySlaytanic <gianni.costanzi@gmail.com>
2022-11-26 19:02:16 +0100

    Initial commit

---
+++ pve_disks_stats_to_influxdb2.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python3
+
+import re
+import sys
+import json
+import argparse
+from datetime import datetime
+from os import popen,getenv
+
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client.client.exceptions import InfluxDBError
+
+INFLUX_HOST = getenv("INFLUX_HOST")
+INFLUX_PORT = getenv("INFLUX_PORT")
+INFLUX_TOKEN = getenv("INFLUX_TOKEN")
+INFLUX_ORGANIZATION = getenv("INFLUX_ORGANIZATION")
+INFLUX_BUCKET = getenv("INFLUX_BUCKET")
+HOST = getenv("HOST_TAG")
+DATA_UNITS_READ_BASE = int(getenv("DATA_UNITS_READ_BASE"))
+DATA_UNITS_WRITTEN_BASE = int(getenv("DATA_UNITS_WRITTEN_BASE"))
+
+DEVICES = {
+    "nvme" : "/dev/nvme0",
+    "sata" : "/dev/sda",
+}
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(usage="PVE Disks Stats to influxdb2 uploader")
+
+    parser.add_argument(
+        "-t",
+        "--test",
+        help="Just print the results without uploading to influxdb2",
+        action="store_true"
+    )
+
+    args = parser.parse_args()
+
+    measurements = []
+    for devtype, devpath in DEVICES.items():
+
+        if devtype == "nvme":
+            stats = {}
+            output_stream = popen(f"/usr/sbin/nvme smart-log {devpath}")
+
+            for line in output_stream:
+                match_found = re.match(r".*Smart Log.*|^[^_]+$", line)
+                temperature_found = re.match(r".*temperature.*", line)
+
+                if not match_found or temperature_found:
+                    key, value = line.strip().split(":")
+                    key = key.strip()
+                    value = value.strip().replace(",","")
+                    value = value.strip().replace("%","")
+
+                    if temperature_found:
+                        value, _ = value.split(" ") 
+                    
+                    if value.isnumeric():
+                        value = int(value)
+
+                    match_found = re.match(r"data_units_read|data_units_written",key)
+
+                    if match_found:
+                        # data_units_read/written is in thousands of 512 bytes blocks
+                        value = value * 512000
+
+                        if key == "data_units_read":
+                            stats["data_units_read_from_day1"] = value
+                            value = value - DATA_UNITS_READ_BASE
+
+                        if key == "data_units_written":
+                            stats["data_units_written_from_day1"] = value
+                            value = value - DATA_UNITS_WRITTEN_BASE
+                    
+                    stats[key.lower()] = value
+
+            output_stream.close()
+
+            measurements.append({
+                "measurement": "disks",
+                "tags": {"host": HOST, "devtype": devtype, "devpath": devpath},
+                "fields": stats
+            })
+
+        elif devtype == "sata":
+            stats = {}
+            output_stream = popen(f"/usr/sbin/smartctl -A {devpath}")
+
+            for line in output_stream:
+                # Line sample:
+                #   1 Raw_Read_Error_Rate     0x002f   100   100   000    Pre-fail  Always       -       0
+                match_found = re.match(r"^\s*[0-9]+\s+([^\s]+).*\s-\s+([^\s]+).*", line)
+
+                if match_found:
+                    stats[match_found.group(1).lower()] = int(match_found.group(2))
+
+            output_stream.close()
+
+            measurements.append({
+                "measurement": "disks",
+                "tags": {"host": HOST, "devtype": devtype, "devpath": devpath},
+                "fields": stats
+            })
+
+    if args.test:
+        print(f"\nMeasurements for host {HOST}")
+        print(json.dumps(measurements, indent=4))
+    else:
+        try:
+            client = InfluxDBClient(url=f"http://{INFLUX_HOST}:{INFLUX_PORT}", token=INFLUX_TOKEN, org=INFLUX_ORGANIZATION, timeout=30000)
+            write_api = client.write_api(write_options=SYNCHRONOUS)
+
+            write_api.write(
+                INFLUX_BUCKET,
+                INFLUX_ORGANIZATION,
+                measurements
+            )
+
+        except TimeoutError as e:
+            failure = True
+            print(e,file=sys.stderr)
+            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] TimeoutError: Could not upload data to {INFLUX_HOST}:{INFLUX_PORT} for host {HOST}",file=sys.stderr)
+            exit(-1)
+        except InfluxDBError as e:
+            failure = True
+            print(e,file=sys.stderr)
+            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] InfluxDBError: Could not upload data to {INFLUX_HOST}:{INFLUX_PORT} for host {HOST}",file=sys.stderr)
+            exit(-1)
+        except Exception as e:
+            failure = True
+            print(e, file=sys.stderr)
+            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Connection Error: Could not upload data to {INFLUX_HOST}:{INFLUX_PORT} for host {HOST}",file=sys.stderr)
+            exit(-1)
+
+        client.close()
---
+++ pve_temp_stats_to_influxdb2.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+
+import re
+import sys
+import json
+import argparse
+from datetime import datetime
+from os import popen,getenv
+
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client.client.exceptions import InfluxDBError
+
+INFLUX_HOST = getenv("INFLUX_HOST")
+INFLUX_PORT = getenv("INFLUX_PORT")
+INFLUX_TOKEN = getenv("INFLUX_TOKEN")
+INFLUX_ORGANIZATION = getenv("INFLUX_ORGANIZATION")
+INFLUX_BUCKET = getenv("INFLUX_BUCKET")
+HOST = getenv("HOST_TAG")
+
+CORE_OFFSET=2
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(usage="PVE Sensors Stats to influxdb2 uploader")
+
+    parser.add_argument(
+        "-t",
+        "--test",
+        help="Just print the results without uploading to influxdb2",
+        action="store_true"
+    )
+
+    args = parser.parse_args()
+
+    measurements = []
+    stats = {}
+
+    data = json.load(popen("/usr/bin/sensors -j"))
+
+    stats["cpu-package"] = int(data["coretemp-isa-0000"]["Package id 0"]["temp1_input"])  
+
+    for index in range(0,6):
+        stats[f"core{index}"] = int(data["coretemp-isa-0000"][f"Core {index}"][f"temp{index+CORE_OFFSET}_input"])
+
+    stats["pch"] = int(data["pch_cannonlake-virtual-0"]["temp1"]["temp1_input"])
+    stats["acpitz"] = int(data["acpitz-acpi-0"]["temp2"]["temp2_input"])
+    stats["nvme-pci"] = int(data["nvme-pci-3a00"]["Composite"]["temp1_input"])
+
+    measurements.append({
+        "measurement": "temp",
+        "tags": {"host": HOST, "service": "lm_sensors"},
+        "fields": stats
+    })
+
+    if args.test:
+        print(f"\nMeasurements for host {HOST}")
+        print(json.dumps(measurements, indent=4))
+    else:
+        try:
+            client = InfluxDBClient(url=f"http://{INFLUX_HOST}:{INFLUX_PORT}", token=INFLUX_TOKEN, org=INFLUX_ORGANIZATION, timeout=30000)
+            write_api = client.write_api(write_options=SYNCHRONOUS)
+
+            write_api.write(
+                INFLUX_BUCKET,
+                INFLUX_ORGANIZATION,
+                measurements
+            )
+
+        except TimeoutError as e:
+            failure = True
+            print(e,file=sys.stderr)
+            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] TimeoutError: Could not upload data to {INFLUX_HOST}:{INFLUX_PORT} for host {HOST}",file=sys.stderr)
+            exit(-1)
+        except InfluxDBError as e:
+            failure = True
+            print(e,file=sys.stderr)
+            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] InfluxDBError: Could not upload data to {INFLUX_HOST}:{INFLUX_PORT} for host {HOST}",file=sys.stderr)
+            exit(-1)
+        except Exception as e:
+            failure = True
+            print(e, file=sys.stderr)
+            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Connection Error: Could not upload data to {INFLUX_HOST}:{INFLUX_PORT} for host {HOST}",file=sys.stderr)
+            exit(-1)
+
+        client.close()
